d4c72e04bbc2894658975fb071d19c9b2f9f44b7
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-02-23 23:09:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,15 @@ function nowServing ( arr ) {
   if (arr.length === 0) {
     return 'There is nobody waiting to be served!';
   }
-  
+  return arr.shift();
+}
+
+function currentLine ( arr ) {
+  if (arr.length === 0) {
+    return 'The line is currently empty.'
+  }
+  var str = 'Thie line is currently:';
+  for (var i=0; i< arr.length; ++i) {
+    str += " " + i+1 ". " + arr[i];
+  }
 }
